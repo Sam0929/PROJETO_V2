603a6c440899405a6a524757fd4f8f10f796f224
GUILHERME LOPES SILVA <guilherme.ls8@puccampinas.edu.br>
2022-11-08 09:30:10 -0300

    sistema de login na pagina inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,20 +5,6 @@
 @section('content')
 
 <!--Cabeçalho-->
-<div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4">Banco de Alunos</span>
-      </a>
-
-            <ul class="nav nav-pills">
-                <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Home</a></li>
-                <li class="nav-item"><a href="alunos" class="nav-link">Tabela de Alunos</a></li>
-            </ul>
-    
-    </header>
-</div>
 
 <!--Conteúdo-->
 <div class='container text-center'>
